47a47e963537e87f9ed1457437d4935eaf639c11
paulaggonzalez <paugonzalez01@hotmail.com>
2023-02-19 19:04:03 -0500

    Actualizacion V3 con html

--- AppCoder/views.py
+++ AppCoder/views.py
@@ -4,16 +4,21 @@ from AppCoder.models import Curso
 
 # Create your views here.
 def inicios (request):
-    return HttpResponse("Vista inicio")
+    return render(request, "AppCoder/inicio.html")
+    #return HttpResponse("Vista inicio")
 
 def cursos (request):
-    return HttpResponse("Vista cursos")
+    return render(request, "AppCoder/cursos.html")
+    #return HttpResponse("Vista cursos")
 
 def profesores (request):
-    return HttpResponse("Vista profesores")
+    return render(request, "AppCoder/profesores.html")
+    #return HttpResponse("Vista profesores")
 
 def entregables (request):
-    return HttpResponse("Vista entregables")
+    return render(request, "AppCoder/entregables.html")
+    #return HttpResponse("Vista entregables")
 
 def estudiantes (request):
-    return HttpResponse("Vista estudiantes")
+    return render(request, "AppCoder/estudiantes.html")
+    #return HttpResponse("Vista estudiantes")
